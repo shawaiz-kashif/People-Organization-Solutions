8f0c7515c4f5f8788e8f8affe731e9b48c005ae4
M.Shawaiz Kashif <131727859+shawaiz-kashif@users.noreply.github.com>
2026-05-20 10:34:42 +0500

    6th  commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <footer>
       <Image
-        src="https://peopleorgsolutions.com/wp-content/uploads/2026/01/SiteLogo-PO-190x121.png"
+        src="/images/POS-Logo.png"
         alt="People Org Solutions"
-        width={200}
-        height={96}
+        width={430}
+        height={100}
       />
       <p>123 Fifth Avenue, NY 10160, New York, USA</p>
       <p>Phone: 800-123-456 | Email: contact@example.com</p>
